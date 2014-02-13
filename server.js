556e47241da85bd8775ac5d01b3426f2a6e7f3a3
gump2012 <yige2002@tom.com>
2014-02-13 17:59:10 +0800

    modify port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ function start(route,handle){
         route(handle,pathname,response,request);
     }
 
-    http.createServer(onRequest).listen(8888);
+    http.createServer(onRequest).listen(10080);
     console.log("server has started");
 }
 
