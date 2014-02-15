06b3b0159a7bbb692e8e57fdac9197025f1400c7
gump2012 <yige2002@tom.com>
2014-02-15 20:46:51 +0800

    modify

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,28 +2,13 @@
  * Created by lishiming on 1/16/14.
  */
 var querystring = require("querystring"),
-    fs = require("fs"),
-    formidable = require("formidable");
+    fs = require("fs");
 
 function start(response,request){
     console.log("Request handler 'start' was called.");
 
-    var body = '<html>' +
-        '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
-        '</head>' +
-        '<body>' +
-        '<form action="/upload" enctype="multipart/form-data" ' +
-        'method="post">' +
-        '<input type="file" name="upload" multiple="multiple">' +
-        '<input type="submit" value="Upload file" />' +
-        '</form>' +
-        '</body>' +
-        '</html>';
-
     response.writeHead(200,{"Content-Type":"text/html"});
-    response.write(body);
+    response.write("hello");
     response.end();
 }
 
@@ -42,21 +27,5 @@ function upload(response,request){
     });
 }
 
-function show(response,request){
-    console.log("Request handler 'show' was called.");
-    fs.readFile("/Users/lishiming/Desktop/gwork/node/jjtest/tmp/weichao.jpg","binary",function(error,file){
-        if(error){
-            response.writeHead(500,{"Content-Type":"text/plain"});
-            response.write(error + "\n");
-            response.end();
-        }else{
-            response.writeHead(200,{"Content-Type":"image/png"});
-            response.write(file,"binary");
-            response.end();
-        }
-    });
-}
-
 exports.start = start;
 exports.upload = upload;
-exports.show = show;
