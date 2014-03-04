a866d932c427fae207acb1b1524a0a99b6706a3a
gump2012 <yige2002@tom.com>
2014-03-04 11:29:40 +0800

    添加iOS测试文件

---
+++ ios/httptest/main.m
@@ -0,0 +1,88 @@
+//
+//  main.m
+//  texthttp
+//
+//  Created by lazybone on 3/4/14.
+//  Copyright (c) 2014 lazybone. All rights reserved.
+//
+
+static NSManagedObjectModel *managedObjectModel()
+{
+    static NSManagedObjectModel *model = nil;
+    if (model != nil) {
+        return model;
+    }
+    
+    NSString *path = @"texthttp";
+    path = [path stringByDeletingPathExtension];
+    NSURL *modelURL = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"momd"]];
+    model = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    
+    return model;
+}
+
+static NSManagedObjectContext *managedObjectContext()
+{
+    static NSManagedObjectContext *context = nil;
+    if (context != nil) {
+        return context;
+    }
+
+    @autoreleasepool {
+        context = [[NSManagedObjectContext alloc] init];
+        
+        NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:managedObjectModel()];
+        [context setPersistentStoreCoordinator:coordinator];
+        
+        NSString *STORE_TYPE = NSSQLiteStoreType;
+        
+        NSString *path = [[NSProcessInfo processInfo] arguments][0];
+        path = [path stringByDeletingPathExtension];
+        NSURL *url = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"sqlite"]];
+        
+        NSError *error;
+        NSPersistentStore *newStore = [coordinator addPersistentStoreWithType:STORE_TYPE configuration:nil URL:url options:nil error:&error];
+        
+        if (newStore == nil) {
+            NSLog(@"Store Configuration Failure %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
+        }
+    }
+    return context;
+}
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        // Create the managed object context
+        NSManagedObjectContext *context = managedObjectContext();
+        
+        // Custom code here...
+        // Save the managed object context
+        NSError *error = nil;
+        if (![context save:&error]) {
+            NSLog(@"Error while saving %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
+            exit(1);
+        }
+        
+        
+        
+        NSURL *url = [NSURL URLWithString:@"http://115.28.225.137:10080/book/login"];
+        //NSURL *url = [NSURL URLWithString:@"http://localhost:10080/book/login"];
+        //第二步，创建请求
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
+        [request setHTTPMethod:@"POST"];//设置请求方式为POST，默认为GET
+        NSString *str = @"{\"mail\":\"yige2002@tom.com\",\"ps\":\"111111\"}";//设置参数
+        NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
+        [request setHTTPBody:data];
+        //第三步，连接服务器
+        
+        NSData *received = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+        
+        NSString *str1 = [[NSString alloc]initWithData:received encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"%@",str1);
+    }
+    return 0;
+}
+
