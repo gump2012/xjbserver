49506118cd9b37b427cd7b2fec6480213e2a29d1
gump2012 <yige2002@tom.com>
2014-03-29 14:49:21 +0800

    更新ios测试程序

--- ios/httptest/main.m
+++ ios/httptest/main.m
@@ -6,83 +6,65 @@
 //  Copyright (c) 2014 lazybone. All rights reserved.
 //
 
-static NSManagedObjectModel *managedObjectModel()
+void testFun(NSString *content ,NSString *strurl)
 {
-    static NSManagedObjectModel *model = nil;
-    if (model != nil) {
-        return model;
-    }
+    NSURL *url = [NSURL URLWithString:strurl];
+    //第二步，创建请求
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
+    [request setHTTPMethod:@"POST"];//设置请求方式为POST，默认为GET
+    NSData *data = [content dataUsingEncoding:NSUTF8StringEncoding];
+    [request setHTTPBody:data];
+    //第三步，连接服务器
     
-    NSString *path = @"texthttp";
-    path = [path stringByDeletingPathExtension];
-    NSURL *modelURL = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"momd"]];
-    model = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    NSData *received = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
     
-    return model;
-}
-
-static NSManagedObjectContext *managedObjectContext()
-{
-    static NSManagedObjectContext *context = nil;
-    if (context != nil) {
-        return context;
-    }
-
-    @autoreleasepool {
-        context = [[NSManagedObjectContext alloc] init];
-        
-        NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:managedObjectModel()];
-        [context setPersistentStoreCoordinator:coordinator];
-        
-        NSString *STORE_TYPE = NSSQLiteStoreType;
-        
-        NSString *path = [[NSProcessInfo processInfo] arguments][0];
-        path = [path stringByDeletingPathExtension];
-        NSURL *url = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"sqlite"]];
-        
-        NSError *error;
-        NSPersistentStore *newStore = [coordinator addPersistentStoreWithType:STORE_TYPE configuration:nil URL:url options:nil error:&error];
-        
-        if (newStore == nil) {
-            NSLog(@"Store Configuration Failure %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
-        }
-    }
-    return context;
+    NSString *str1 = [[NSString alloc]initWithData:received encoding:NSUTF8StringEncoding];
+    
+    NSLog(@"%@",str1);
 }
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        // Create the managed object context
-        NSManagedObjectContext *context = managedObjectContext();
-        
-        // Custom code here...
-        // Save the managed object context
-        NSError *error = nil;
-        if (![context save:&error]) {
-            NSLog(@"Error while saving %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
-            exit(1);
-        }
         
         
         
-        NSURL *url = [NSURL URLWithString:@"http://115.28.225.137:10080/book/login"];
+        //NSURL *url = [NSURL URLWithString:@"http://115.28.225.137:10080/book/login"];
         //NSURL *url = [NSURL URLWithString:@"http://localhost:10080/book/login"];
-        //第二步，创建请求
-        NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10];
-        [request setHTTPMethod:@"POST"];//设置请求方式为POST，默认为GET
-        NSString *str = @"{\"mail\":\"yige2002@tom.com\",\"ps\":\"111111\"}";//设置参数
-        NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding];
-        [request setHTTPBody:data];
-        //第三步，连接服务器
+        //NSString *str = @"{\"mail\":\"yige2002@tom.com\",\"ps\":\"111111\"}";//设置参数
+        NSString *strurl = @"http://115.28.225.137:10080/order?assistant=neworder";
+        NSString *strc =@"\
+{\
+\"city\": \"2268\",\
+\"consignee\": \"李东\",\
+\"mobile\": \"13646426253\",\
+\"memo\": \"你好\",\
+\"province\": \"2182\",\
+\"ticket_id\": \"bb2dd06d40c0fd6cc8b291e8f26e7764\",\
+\"token\": \"5a928f51b477a1162cf7c767b5a7dbda\",\
+\"address\": \"青大纺织学院东门\",\
+\"shipping_fee\": \"12.00\",\
+\"promotion_totalprice\": \"22.00\",\
+\"area\": \"2275\",\
+\"payment_way_id\": \"1\",\
+\"productlist\": [\
+{\
+\"title\": \"好产品\",\
+\"attr_list\": [\
+{\"goods_attr_id\":\"2828\",\
+\"attr_price\":\"0.00\"}\
+],\
+\"price\": \"22\",\
+\"pid\": \"325\",\
+\"quantity\": \"1\"\
+}\
+]\
+}";
+        testFun(strc,strurl);
         
-        NSData *received = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-        
-        NSString *str1 = [[NSString alloc]initWithData:received encoding:NSUTF8StringEncoding];
-        
-        NSLog(@"%@",str1);
     }
     return 0;
 }
+
 
