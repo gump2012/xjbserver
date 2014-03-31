224890c6eb468767d21606ebd25fe0d49a5c3d16
gump2012 <yige2002@tom.com>
2014-03-31 21:24:12 +0800

    修改一个url无数据崩溃bug

--- book/login/bookLoginHandlers.js
+++ book/login/bookLoginHandlers.js
@@ -13,34 +13,42 @@ function bookLogin(response,request){
     });
 
     request.addListener('end', function() {
-        var datajson = JSON.parse(requestData);
-        console.log(datajson.mail,datajson.ps);
-        var postData = '';
-        var bookuser = mongoose.model('user');
-        bookuser.find({mail:datajson.mail},function(err,buser){
-            if(buser.length > 0)
-            {
-                var dbuser = buser[0];
-                if(dbuser.ps == datajson.ps)
+
+        if(requestData != ''){
+            var datajson = JSON.parse(requestData);
+            console.log(datajson.mail,datajson.ps);
+            var postData = '';
+            var bookuser = mongoose.model('user');
+            bookuser.find({mail:datajson.mail},function(err,buser){
+                if(buser.length > 0)
                 {
-                    postData = JSON.stringify({booklogin:'1'});
+                    var dbuser = buser[0];
+                    if(dbuser.ps == datajson.ps)
+                    {
+                        postData = JSON.stringify({booklogin:'1'});
+                    }
+                    else
+                    {
+                        postData = JSON.stringify({booklogin:'2'});
+                    }
+
                 }
                 else
                 {
-                    postData = JSON.stringify({booklogin:'2'});
+                    postData = JSON.stringify({booklogin:'0'});
                 }
 
-            }
-            else
-            {
-                postData = JSON.stringify({booklogin:'0'});
-            }
-
-            response.writeHead(200,{"Content-Type":"text/html"});
-            response.write(postData);
-            console.log(postData);
+                response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+                response.write(postData);
+                console.log(postData);
+                response.end();
+            });
+        }
+        else{
+            response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+            response.write('木有数据');
             response.end();
-        });
+        }
     });
 }
 
@@ -53,35 +61,43 @@ function bookRegister(response,request){
     });
 
     request.addListener('end', function() {
-        var datajson = JSON.parse(requestData);
-        console.log(datajson.mail,datajson.ps);
-        var postData = '';
-        var bookuser = mongoose.model('user');
-        bookuser.find({mail:datajson.mail},function(err,buser){
-            if(buser.length > 0)
-            {
-                postData = JSON.stringify({register:'0'});
-            }
-            else
-            {
-                var newuser = new bookuser({mail:datajson.mail, ps:datajson.ps});
 
-                //保存实例
-                newuser.save( function( err, silence ) {
-                    if( err )
-                    {
-                        console.log(err);
-                    }
-                });
+        if(requestData != ''){
+            var datajson = JSON.parse(requestData);
+            console.log(datajson.mail,datajson.ps);
+            var postData = '';
+            var bookuser = mongoose.model('user');
+            bookuser.find({mail:datajson.mail},function(err,buser){
+                if(buser.length > 0)
+                {
+                    postData = JSON.stringify({register:'0'});
+                }
+                else
+                {
+                    var newuser = new bookuser({mail:datajson.mail, ps:datajson.ps});
 
-                postData = JSON.stringify({register:'1'});
-            }
+                    //保存实例
+                    newuser.save( function( err, silence ) {
+                        if( err )
+                        {
+                            console.log(err);
+                        }
+                    });
 
-            response.writeHead(200,{"Content-Type":"text/html"});
-            response.write(postData);
-            console.log(postData);
+                    postData = JSON.stringify({register:'1'});
+                }
+
+                response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+                response.write(postData);
+                console.log(postData);
+                response.end();
+            });
+        }
+        else{
+            response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+            response.write('木有数据');
             response.end();
-        });
+        }
     });
 }
 
