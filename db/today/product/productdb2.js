e4aeb8323bf0238e11f95f31c96f424eb87f13ea
gump2012 <yige2002@tom.com>
2014-04-12 15:47:53 +0800

    添加cid2商品数据

---
+++ db/today/product/productdb2.js
@@ -0,0 +1,369 @@
+/**
+ * Created by gump on 4/12/14.
+ */
+
+function creatdb(mongoose){
+    var todayCategory = mongoose.model('todayProduct');
+
+    var newCategory = new todayCategory({pid:21
+        ,cid:2
+        ,title:'EVE G爽润滑油 阴交 肛交用润滑油 一滑到底'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_1.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:22
+        ,cid:2
+        ,title:'JOKER外用延时喷剂日本史上最久 最受欢迎'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_2.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:23
+        ,cid:2
+        ,title:'MFPONES男用助勃刺激阴蒂震动环 世界好套环'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_3.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:24
+        ,cid:2
+        ,title:'倍耐力延时喷剂纯植物提取 让男人更从容自信'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_4.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:25
+        ,cid:2
+        ,title:'印度神油印度秘方久战不倒杀菌防病延时金刚'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_5.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:26
+        ,cid:2
+        ,title:'头牌推荐 百倍安全管理 私密保存'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_6.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:27
+        ,cid:2
+        ,title:'头牌推荐 耐氏男用延时喷剂 超久防早泄 正品防伪'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_7.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:28
+        ,cid:2
+        ,title:'延时必备 三色锁精环 根本射不出来'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_8.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:29
+        ,cid:2
+        ,title:'杜蕾斯按摩油 润滑油 让爱更浓烈'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_9.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:30
+        ,cid:2
+        ,title:'正品罪爱费洛蒙香水佛罗蒙调助情兴奋男用女用迷人强效催性欲特价'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_10.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:31
+        ,cid:2
+        ,title:'水晶之恋凤梨套 延时增粗 G点 刺激 宝贝好喜欢'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_11.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:32
+        ,cid:2
+        ,title:'爱妃液人体爱液润滑油 按摩推油 可用肛交'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_12.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:33
+        ,cid:2
+        ,title:'玩爆潮品 12星座延时锁精水晶套 G点阴道刺激异型套'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_13.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:34
+        ,cid:2
+        ,title:'玩爆潮品男用龟头增大增粗延时套'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_14.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:35
+        ,cid:2
+        ,title:'男用阴茎增粗增大增长狼牙套 伴君如伴虎'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_15.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:36
+        ,cid:2
+        ,title:'百乐男用阴茎增粗增大增长狼牙套 各种刺激伴君左右'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_16.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:37
+        ,cid:2
+        ,title:'纯天然高级羊眼圈 让您的高潮永驻'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_17.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:38
+        ,cid:2
+        ,title:'美国Enlarge男士阴茎增粗助大精油 外用防早泄'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_18.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:39
+        ,cid:2
+        ,title:'耐氏外用延时湿巾 天然最安全（1片12片'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_19.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:40
+        ,cid:2
+        ,title:'菲尔普成人用品玩具专用消毒液 深度清洁 私密防护'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:18.00
+        ,price:9.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/categoryproduct2_20.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+}
+
+exports.createdb = creatdb;
