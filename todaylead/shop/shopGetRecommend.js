7c87dd74a2b44598f4f2557f75848b8f4da574c8
gump2012 <yige2002@tom.com>
2014-04-18 07:15:23 +0800

    添加推荐商品协议

---
+++ todaylead/shop/shopGetRecommend.js
@@ -0,0 +1,60 @@
+/**
+ * Created by gump on 4/18/14.
+ */
+
+var mongoose = require('mongoose');
+
+function getRecommend(response,request){
+
+    var productmodle = mongoose.model('todayProduct');
+
+    var strRecommend = '新潮推荐';
+    var strkw = '新潮';
+    var responsevalue = {
+        info:{
+            extra:{},
+            data:{
+                title:strRecommend
+                ,keyword:strkw
+                ,topthree:[]
+            }
+        },
+        response_status:'',
+        msg:''
+    }
+
+    productmodle.find({},'title pid pic_url',{sort: {pid:'desc'}}, function (err, docs) {
+        var gettext=[];
+        for(var i in docs){
+            if(docs[i].title.indexOf(strkw) != -1){
+                var item = {
+                    pic_url:docs[i].pic_url
+                    ,pid:docs[i].pid
+                }
+                gettext.push(item);
+            }
+        }
+
+        if(gettext.length > 2)
+        {
+            for(var i = 0; i < 3; ++i)
+            {
+                responsevalue.info.data.topthree.push(gettext[i]);
+            }
+        }
+        else
+        {
+            for(i in gettext)
+            {
+                responsevalue.info.data.topthree.push(gettext[i]);
+            }
+        }
+
+        var postData = JSON.stringify(responsevalue);
+        response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+        response.write(postData);
+        response.end();
+    });
+}
+
+exports.getRecommend = getRecommend;
--- todaylead/shop/shopHandles.js
+++ todaylead/shop/shopHandles.js
@@ -5,9 +5,11 @@ var getAssistantValue = require("../todayPublic/getAssistantValue");
 var route = require("../../router");
 
 var shopGetAdList = require("./shopGetAdList");
+var shopGetRecommend = require("./shopGetRecommend");
 
 var adHandle = {}
 adHandle["getAdList"] = shopGetAdList.getAdList;
+adHandle["getRecommend"] = shopGetRecommend.getRecommend;
 
 function ad(response,request){
     var assistant = getAssistantValue.getAssistantValue(request);
