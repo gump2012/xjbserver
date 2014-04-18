ba07743ae37810b22abaf60f5732507afaa51ff7
gump2012 <yige2002@tom.com>
2014-04-18 21:33:50 +0800

    添加分类6数据

--- db/today/product/productdb.js
+++ db/today/product/productdb.js
@@ -7,6 +7,7 @@ var productdb1 = require('./productdb1');
 var productdb3 = require('./productdb3');
 var productdb4 = require('./productdb4');
 var productdb5 = require('./productdb5');
+var productdb6 = require('./productdb6');
 
 function creatdb(mongoose){
     var proSchema = new mongoose.Schema({
@@ -38,6 +39,7 @@ function creatdb(mongoose){
     productdb3.createdb(mongoose);
     productdb4.createdb(mongoose);
     productdb5.createdb(mongoose);
+    productdb6.createdb(mongoose);
 }
 
 exports.createdb = creatdb;
---
+++ db/today/product/productdb6.js
@@ -0,0 +1,368 @@
+/**
+ * Created by gump on 4/18/14.
+ */
+function creatdb(mongoose){
+    var todayCategory = mongoose.model('todayProduct');
+
+    var newCategory = new todayCategory({pid:101
+        ,cid:6
+        ,title:'倍力乐销魂掌 全能调情手套 按摩潮吹 女用助情自慰 前戏情趣用品'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:38.00
+        ,price:19.9
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_1.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:102
+        ,cid:6
+        ,title:'后庭塞 肛塞 肛门塞 拉珠 男用女用肛门自慰g点后庭情趣用品'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:20.00
+        ,price:8.80
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_2.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:103
+        ,cid:6
+        ,title:'女用震动挑逗自慰情趣拉珠调情棒肛塞 G点后庭葫芦钻'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:48.00
+        ,price:19.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_3.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:120
+        ,cid:6
+        ,title:'香港邦爱另类玩具sm情趣双峰震动乳夹女用品乳房头刺激自慰按摩器'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:72.00
+        ,price:49.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_20.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:104
+        ,cid:6
+        ,title:'成人情趣用品夫妻前戏调情白色透明夜光骰子筛子色子房事性爱助情'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:9.00
+        ,price:3.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_4.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:105
+        ,cid:6
+        ,title:'正品 倍力乐威震天震震嘴 男女共用五种用法口交器'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:280.00
+        ,price:165.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_5.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:106
+        ,cid:6
+        ,title:'正品SEMO龙凤拉珠'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:38.00
+        ,price:19.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_6.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:107
+        ,cid:6
+        ,title:'正品蒂贝DIBE电动舌头口交器 变频震动棒跳蛋 女用自慰器具G点刺激棒'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:158.00
+        ,price:98.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_7.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:108
+        ,cid:6
+        ,title:'玩爆潮品 硅胶拉珠女用后庭链肝门塞情趣用品成人玩具G点刺激棒'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:48.00
+        ,price:19.90
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_8.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:109
+        ,cid:6
+        ,title:'百乐正品振翅蝴蝶女用口交器吸水高潮不断情趣用品'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:78.00
+        ,price:36.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_09.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:110
+        ,cid:6
+        ,title:'百乐电击器 电动乳房胸部按摩贴 阴贴 情趣多功能电磁脉冲器'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:100.00
+        ,price:50.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_10.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:111
+        ,cid:6
+        ,title:'百乐电动假舌头舔吸阴蒂口交 情趣成人女用高潮吹刺激自慰性用器'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:120.00
+        ,price:69.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_11.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:112
+        ,cid:6
+        ,title:'百乐震动乳夹 前戏必备 男用女用自慰器 乳头刺激 电动乳房按摩器'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:80.00
+        ,price:45.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_12.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+    var newCategory = new todayCategory({pid:113
+        ,cid:6
+        ,title:'震动阴道窥视镜女用阴道内窥镜 窥阴器乐透镜 扩阴器情趣另类玩具'
+        ,volume:0
+        ,recentvolume:0
+        ,org_price:138.00
+        ,price:55.00
+        ,state:1
+        ,stamper:''
+        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_13.jpg'
+        ,time:0});
+    newCategory.save( function( err, silence ) {
+        if( err )
+        {
+            console.log(err);
+        }
+    });
+
+//    var newCategory = new todayCategory({pid:115
+//        ,cid:6
+//        ,title:'新潮套套 倍力乐毛毛虫震动刺激套 震动器毛毛虫'
+//        ,volume:0
+//        ,recentvolume:0
+//        ,org_price:30.00
+//        ,price:9.50
+//        ,state:1
+//        ,stamper:''
+//        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_15.jpg'
+//        ,time:0});
+//    newCategory.save( function( err, silence ) {
+//        if( err )
+//        {
+//            console.log(err);
+//        }
+//    });
+//
+//    var newCategory = new todayCategory({pid:116
+//        ,cid:6
+//        ,title:'杜蕾斯大胆爱10片装 给您无限激情'
+//        ,volume:0
+//        ,recentvolume:0
+//        ,org_price:39.00
+//        ,price:19.00
+//        ,state:1
+//        ,stamper:''
+//        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_16.jpg'
+//        ,time:0});
+//    newCategory.save( function( err, silence ) {
+//        if( err )
+//        {
+//            console.log(err);
+//        }
+//    });
+//
+//    var newCategory = new todayCategory({pid:117
+//        ,cid:6
+//        ,title:'杜蕾斯活力装 12只安全套 活力四射'
+//        ,volume:0
+//        ,recentvolume:0
+//        ,org_price:65.00
+//        ,price:33.00
+//        ,state:1
+//        ,stamper:''
+//        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_17.jpg'
+//        ,time:0});
+//    newCategory.save( function( err, silence ) {
+//        if( err )
+//        {
+//            console.log(err);
+//        }
+//    });
+//
+//    var newCategory = new todayCategory({pid:118
+//        ,cid:6
+//        ,title:'欧米加 夜店果冻装避孕套随机单片 超薄大油量清香怡人'
+//        ,volume:0
+//        ,recentvolume:0
+//        ,org_price:76.00
+//        ,price:65.00
+//        ,state:1
+//        ,stamper:''
+//        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_18.jpg'
+//        ,time:0});
+//    newCategory.save( function( err, silence ) {
+//        if( err )
+//        {
+//            console.log(err);
+//        }
+//    });
+//
+//    var newCategory = new todayCategory({pid:119
+//        ,cid:6
+//        ,title:'正品冈本授权OK系列 10只装'
+//        ,volume:0
+//        ,recentvolume:0
+//        ,org_price:45.00
+//        ,price:18.00
+//        ,state:1
+//        ,stamper:''
+//        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_19.jpg'
+//        ,time:0});
+//    newCategory.save( function( err, silence ) {
+//        if( err )
+//        {
+//            console.log(err);
+//        }
+//    });
+//
+//    var newCategory = new todayCategory({pid:120
+//        ,cid:6
+//        ,title:'第六感快乐家族 螺纹 冰火 颗粒 超薄 芦荟 低价疯狂来袭'
+//        ,volume:0
+//        ,recentvolume:0
+//        ,org_price:38.00
+//        ,price:12.00
+//        ,state:1
+//        ,stamper:''
+//        ,pic_url:'http://115.28.225.137:10080/res/images/category/product/category6/categoryproduct6_20.jpg'
+//        ,time:0});
+//    newCategory.save( function( err, silence ) {
+//        if( err )
+//        {
+//            console.log(err);
+//        }
+//    });
+}
+
+exports.createdb = creatdb;
