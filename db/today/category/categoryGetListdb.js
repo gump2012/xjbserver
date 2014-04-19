c76ad2894a1d5f45e308e4f0fe172f47356531fa
gump2012 <yige2002@tom.com>
2014-04-19 13:02:03 +0800

    修改分类链表连接

--- db/today/category/categoryGetListdb.js
+++ db/today/category/categoryGetListdb.js
@@ -22,7 +22,7 @@ function createdb(mongoose){
     var newCategory = new todayCategory({cid:1
         ,cname:'情趣内衣'
         ,info:'衣带渐宽,为伊憔悴'
-        ,pic_url:'http://115.28.225.137:10080/res/images/category/list/categorylist1.jpg'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DH2G1L6i/6mBiO.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -33,7 +33,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:2
         , cname:'助情催欲'
         ,info:'郎情妾意,干柴烈火'
-        ,pic_url:'http://115.28.225.137:10080/res/images/category/list/categorylist2.jpg'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DH2FpeV3/eeQ12.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -44,7 +44,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:3
         , cname:'男性玩具'
         ,info:'旅途漫漫,夜夜笙歌'
-        ,pic_url:'http://115.28.225.137:10080/res/images/category/list/categorylist3.jpg'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DH2G39a7/Rgc1k.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -55,7 +55,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:4
         , cname:'女性玩具'
         ,info:'头牌佳人,绝世独立'
-        ,pic_url:'http://115.28.225.137:10080/res/images/category/list/categorylist4.jpg'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DH2G1XG4/v9w0o.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -66,7 +66,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:5
         , cname:'套套世界'
         ,info:'超薄 持久 大颗粒 螺纹'
-        ,pic_url:'http://115.28.225.137:10080/res/images/category/list/categorylist5.jpg'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DH2G2cmN/GyxCk.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -77,7 +77,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:6
         , cname:'两性游戏'
         ,info:'超薄 持久 大颗粒 螺纹'
-        ,pic_url:'http://115.28.225.137:10080/res/images/category/list/categorylist6.jpg'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DH3cxwUJ/5cfl8.jpg'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
