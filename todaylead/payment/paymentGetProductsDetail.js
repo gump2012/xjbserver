6b00a5ff2752e03ea26b0cb7957271159b6975bd
gump2012 <yige2002@tom.com>
2014-04-21 17:51:16 +0800

    添加购物获得商品细节协议

--- todaylead/payment/paymentGetProductsDetail.js
+++ todaylead/payment/paymentGetProductsDetail.js
@@ -18,8 +18,16 @@ function getProductsDetail(response,request){
         if(datajson.pids){
             var strpids = datajson.pids;
             var pidsarr=strpids.split(",");
-            console.log(pidsarr.length);
             if(pidsarr.length > 0){
+                var responsevalue = {
+                    info:{
+                        extra:''
+                        ,data:[]
+                    }
+                    ,response_status:''
+                    ,msg:''
+                };
+
                 findProduct(pidsarr,0,response,responsevalue);
             }
             else{
@@ -33,7 +41,65 @@ function getProductsDetail(response,request){
 }
 
 function findProduct(pidarr,iindex,response,responsevalue){
+    var productmodle = mongoose.model('todayProduct');
+    var pid = parseInt(pidarr[iindex]);
+    productmodle.findOne({pid:pid},function(err,doc){
+        if(doc){
+            productitem = {
+                pid:doc.pid
+                ,title:doc.title
+                ,price:doc.price
+                ,state:doc.state
+                ,cid:doc.cid
+                ,attr_list:[]
+            };
 
+            responsevalue.info.data.push(productitem);
+            findAttribute(pidarr,iindex,response,responsevalue);
+        }
+        else{
+            publicfun.returnErr(response,'没有找到商品数据');
+        }
+    });
+}
+
+function findAttribute(pidarr,iindex,response,responsevalue){
+    if(iindex >= responsevalue.info.data.length)
+    {
+        publicfun.returnErr(response,'服务器数据逻辑错误');
+    }
+    else{
+        var productAttrModle = mongoose.model('todayProductAttr');
+
+        productAttrModle.find({pid:responsevalue.info.data[iindex].pid},function(err,doc){
+            for(i in doc){
+                if(doc[i].attr_type == 1){
+                    var item = {
+                        attr_id          :doc[i].attr_id
+                        ,goods_attr_id   :doc[i].goods_attr_id
+                        ,attr_name       :doc[i].attr_name
+                        ,attr_value      :doc[i].attr_value
+                        ,attr_price      :doc[i].attr_price
+                        ,attr_type       :doc[i].attr_type
+                    }
+
+                    responsevalue.info.data[iindex].attr_list.push(item);
+                }
+            }
+
+            iindex++;
+
+            if(iindex >= pidarr.length){
+                var postData = JSON.stringify(responsevalue);
+                response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+                response.write(postData);
+                response.end();
+            }
+            else{
+                findProduct(pidarr,iindex,response,responsevalue);
+            }
+        });
+    }
 }
 
 exports.getProductsDetail = getProductsDetail;
