437d1461830010955dea94c9da9b185b3340c9a7
gump2012 <yige2002@tom.com>
2014-04-22 13:04:52 +0800

    添加获得模块商品详情协议

---
+++ todaylead/shop/shopGetTopicProducts.js
@@ -0,0 +1,83 @@
+/**
+ * Created by lishiming on 14-4-22.
+ */
+var mongoose = require('mongoose');
+
+var url = require("url");
+var querystring = require("querystring");
+
+var publictool = require("../todayPublic/getAssistantValue");
+
+function getTopicPro(response,request){
+
+    var arg = url.parse(request.url).query;
+    var topicid = querystring.parse(arg).topicid;
+
+    var responsevalue = {
+        info:{
+            extra:{},
+            data:[]
+        },
+        response_status:'',
+        msg:''
+    }
+
+    if(topicid){
+        var topicmodle = mongoose.model('todayshoptopic');
+        topicmodle.findOne({topicid:topicid},function(err,doc){
+            if(doc){
+                if(doc.goods.length > 0){
+                    findProduct(doc.goods,0,response,responsevalue);
+                }
+                else{
+                    publictool.returnErr(response,'no goods');
+                }
+            }
+            else{
+                publictool.returnErr(response,'not find topic data');
+            }
+        });
+    }
+    else{
+        publictool.returnErr(response,'not find topic post parameter');
+    }
+}
+
+function findProduct(gooosarr,iindex,response,responsevalue){
+    var productmodle = mongoose.model('todayProduct');
+    productmodle.findOne({pid:gooosarr[iindex].pid},function(err,doc){
+        if(doc)
+        {
+            var item = {
+                pid:doc.pid
+                ,pic_url:doc.pic_url
+                ,title:doc.title
+                ,org_price:doc.org_price
+                ,price:doc.price
+                ,volume:doc.volume
+                ,recentvolume:doc.recentvolume
+            }
+
+            responsevalue.info.data.push(item);
+
+            if(iindex < gooosarr.length - 1)
+            {
+                iindex++;
+                findProduct(gooosarr,iindex,response,responsevalue);
+            }
+            else
+            {
+                var postData = JSON.stringify(responsevalue);
+                response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+                response.write(postData);
+                response.end();
+            }
+        }
+        else
+        {
+            publictool.returnErr(response,'not find product');
+        }
+    });
+}
+
+exports.getTopicPro = getTopicPro;
--- todaylead/shop/shopHandles.js
+++ todaylead/shop/shopHandles.js
@@ -8,12 +8,14 @@ var shopGetAdList = require("./shopGetAdList");
 var shopGetRecommend = require("./shopGetRecommend");
 var shopKeywordSearch = require("./shopKeywordSearch");
 var shopGetTopic = require("./shopGetTopic");
+var shopGetTopPro = require("./shopGetTopicProducts");
 
 var adHandle = {}
 adHandle["getAdList"] = shopGetAdList.getAdList;
 adHandle["getRecommend"] = shopGetRecommend.getRecommend;
 adHandle["keywordSearch"] = shopKeywordSearch.getProducts;
-adHandle["getTopic"] = shopGetTopic.getTopic;
+adHandle["getTopicList"] = shopGetTopic.getTopic;
+adHandle["getTopicProducts"] = shopGetTopPro.getTopicPro;
 
 function ad(response,request){
     var assistant = getAssistantValue.getAssistantValue(request);
