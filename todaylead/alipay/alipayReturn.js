744a15740761090d3fc5b61881d24b24763b4cdc
gump2012 <yige2002@tom.com>
2014-05-19 16:12:34 +0800

    查看支付宝返回

--- todaylead/alipay/alipayReturn.js
+++ todaylead/alipay/alipayReturn.js
@@ -2,10 +2,17 @@
  * Created by lishiming on 14-5-9.
  */
 function alipayreturn(response,request){
+    var requestData = '';
+    request.addListener('data', function(postDataChunk) {
+        requestData += postDataChunk;
+    });
 
-    response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
-    response.write("success");
-    response.end();
+    request.addListener('end', function() {
+        console.log(requestData);
+        response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+        response.write("success");
+        response.end();
+    });
 }
 
 exports.alipayreturn = alipayreturn;
