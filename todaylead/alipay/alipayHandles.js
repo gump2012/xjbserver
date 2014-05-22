96b27e61a2e296fa6cb591537dd30f5b633e91ae
gump2012 <yige2002@tom.com>
2014-05-22 15:07:54 +0800

    修改返回log

--- todaylead/alipay/alipayHandles.js
+++ todaylead/alipay/alipayHandles.js
@@ -4,6 +4,7 @@
 var getAssistantValue = require("../todayPublic/getAssistantValue");
 var route = require("../../router");
 var querystring = require("querystring");
+var mongoose = require('mongoose');
 
 function alipay(response,request){
 
@@ -14,12 +15,44 @@ function alipay(response,request){
 
     request.addListener('end', function() {
         console.log(requestData);
-        console.log(querystring.parse(requestData).out_trade_no);
-        console.log(querystring.parse(requestData).trade_status)
-        response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
-        response.write("success");
-        response.end();
+        var out_trade_no = querystring.parse(requestData).out_trade_no;
+        var trade_status = querystring.parse(requestData).trade_status;
+        console.log(trade_status);
+        if(trade_status){
+            if(trade_status.indexOf('TRADE_FINISHED') || trade_status.indexOf('TRADE_SUCCESS')){
+                if(out_trade_no){
+                    var ordermodle = mongoose.model('todayOrder');
+                    ordermodle.findOne({order_id:out_trade_no},function(err,doc){
+                        if(doc){
+                            doc.order_states = 1;
+                            doc.payment_states = 1;
+                            doc.save( function( err, silence ) {
+                                if( err )
+                                {
+                                    console.log(err);
+                                }
+                            });
+                        }
+                    });
+                }
+                
+                returnText(response,'success');
+            }
+            else{
+                returnText(response,'false');
+            }
+        }
+        else{
+            returnText(response,'false');
+        }
+
     });
+}
+
+function returnText(response,strText){
+    response.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+    response.write(strText);
+    response.end();
 }
 
 exports.alipay = alipay;
