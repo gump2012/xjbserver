b7f1ae52e7aa0c7cd1fddd57d58aa01342b4f226
gump2012 <yige2002@tom.com>
2014-05-26 11:04:15 +0800

    中文2

--- todaylead/order/newOrder.js
+++ todaylead/order/newOrder.js
@@ -258,7 +258,7 @@ function findPaymentName(response,item){
                     '&payment_type="1"' +
                     '&seller_id="toupai@3pshow.com"' +
                     '&service="mobile.securitypay.pay"' +
-                    '&subject="%e9%99%88%e5%9b%bd%e7%94%a8"' +
+                    '&subject="chenguoyong"' +
                     '&total_fee="' + responsevalue.info.data.orderprice + '"';
 
                 makeRsa(orderstr,responsevalue,response);
