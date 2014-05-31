b05bc35141256193d6e04b313c9ce7593f5c2b67
gump2012 <yige2002@tom.com>
2014-05-31 18:31:07 +0800

    ee

--- todaylead/order/orderGetList.js
+++ todaylead/order/orderGetList.js
@@ -83,7 +83,7 @@ function getReturnValue(docs,limit,page,response,responsevalue){
                 for(var i = istartcount;i < docs.length;++i){
                     var item = {
                         order_id:docs[i].order_id
-                        ,orderprice:docs[i].promotion_totalprice + docs[i].shipping_fee
+                        ,order_price:docs[i].promotion_totalprice + docs[i].shipping_fee
                         ,create_time:docs[i].creat_time
                         ,order_status:docs[i].order_states
                         ,pay_status:docs[i].payment_states
