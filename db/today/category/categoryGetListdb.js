7caf460148743c8b5a9d0683ed54bf5d014e28f6
gump2012 <yige2002@tom.com>
2014-06-09 21:51:36 +0800

    分类图片

--- db/today/category/categoryGetListdb.js
+++ db/today/category/categoryGetListdb.js
@@ -22,7 +22,7 @@ function createdb(mongoose){
     var newCategory = new todayCategory({cid:1
         ,cname:'情趣内衣'
         ,info:'衣带渐宽，为伊憔悴，老公赶紧回家啦。'
-        ,pic_url:'http://pic.yupoo.com/yige2002/DMTH3JYl/qBW7D.png'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DOQmTpEj/oBAzJ.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -33,7 +33,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:2
         , cname:'助情催欲'
         ,info:'郎情妾意，干柴烈火，天咋还不黑？'
-        ,pic_url:'http://pic.yupoo.com/yige2002/DMTH4zZE/SYLet.png'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DOQngL5x/8uhFl.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -44,7 +44,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:3
         , cname:'男性玩具'
         ,info:'旅途漫漫，夜夜笙歌，俺不去东莞啦！'
-        ,pic_url:'http://pic.yupoo.com/yige2002/DMTGWAMe/GPwRm.png'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DOQngGaW/ur15O.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -55,7 +55,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:4
         , cname:'女性玩具'
         ,info:'头牌佳人，绝世独立，蛋蛋去哪儿啦！'
-        ,pic_url:'http://pic.yupoo.com/yige2002/DMTGWISY/GvUbJ.png'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DOQnhp5d/u8BRl.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -66,7 +66,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:5
         , cname:'套套世界'
         ,info:'中央一套，人生逍遥，爸爸我是咋来滴？'
-        ,pic_url:'http://pic.yupoo.com/yige2002/DMTH4wpK/7dVXL.png'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DOQmT009/o5mWT.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
@@ -77,7 +77,7 @@ function createdb(mongoose){
     newCategory = new todayCategory({cid:6
         , cname:'两性游戏'
         ,info:'花前月下，你侬我侬'
-        ,pic_url:'http://pic.yupoo.com/yige2002/DMTGVbLL/rUCQj.png'});
+        ,pic_url:'http://pic.yupoo.com/yige2002/DOQngy1N/1ZRx9.png'});
     newCategory.save( function( err, silence ) {
         if( err )
         {
