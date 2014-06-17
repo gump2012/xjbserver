4330e30fedb13bde9fb75d99d1ba7006ff687cb2
gump2012 <yige2002@tom.com>
2014-06-17 14:33:35 +0800

    添加得到快递接口

---
+++ todaylead/order/orderExpress.js
@@ -0,0 +1,46 @@
+/**
+ * Created by lishiming on 14-6-17.
+ */
+var querystring = require("querystring");
+var url = require("url");
+var mongoose = require('mongoose');
+
+var publictool = require("../todayPublic/getAssistantValue");
+
+exports.getExpress = function (response,request){
+
+    var arg = url.parse(request.url).query;
+    var order_id = querystring.parse(arg).order_id;
+
+    if(order_id){
+        var ordermodle = mongoose.model('todayOrder');
+        ordermodle.findOne({order_id:order_id,ticket_id:ticketid},function(err,doc){
+            if(doc){
+                if(doc.express_name && doc.express_number){
+                    var strurl = 'http://m.kuaidi100.com/index_all.html?type='+doc.express_name+'&postid='+doc.express_number;
+
+                    var responseValue = {
+                        info:{
+                            extra:{}
+                            ,data:{
+                                express_url:strurl
+                            }
+                        }
+                        ,response_status:'true'
+                        ,msg:''
+                    }
+                    publictool.returnValue(response,responsevalue);
+                }else{
+                    publictool.returnErr(response,'无快递信息');
+                }
+
+            }
+            else{
+                publictool.returnErr(response,'no order data');
+            }
+        });
+    }
+    else{
+        publictool.returnErr(response,'need orderid');
+    }
+}
--- todaylead/order/orderHandles.js
+++ todaylead/order/orderHandles.js
@@ -9,6 +9,7 @@ var orderGetDetail = require("./orderGetDetail");
 var orderGetList = require("./orderGetList");
 var orderDel = require("./orderDelete");
 var alipayto = require("./alipayto");
+var express = require("./orderExpress");
 
 var orderHandle = {};
 orderHandle["createOrder"] = newOrder.newOrder;
@@ -16,6 +17,7 @@ orderHandle["getOrderDetail"] = orderGetDetail.getMyOrderDetail;
 orderHandle["getOrderList"] = orderGetList.getMyOrderList;
 orderHandle["delOrder"] = orderDel.deleteorder;
 orderHandle["alipayPay"] = alipayto.alipayto;
+orderHandle["getexpress"] = express.getExpress;
 
 function order(response,request){
     var assistant = getAssistantValue.getAssistantValue(request);
