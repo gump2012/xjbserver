aa206d73b656cef49a3dfe90b2eb966e2748b56f
gump2012 <yige2002@tom.com>
2014-06-18 22:49:19 +0800

    修改banner

--- todaylead/todayPublic/urlmacro/macro_shopurl.js
+++ todaylead/todayPublic/urlmacro/macro_shopurl.js
@@ -1,7 +1,7 @@
 /**
  * Created by lishiming on 14-6-10.
  */
-exports.AD_1 = 'http://pic.yupoo.com/yige2002/DJo6DUKq/WurV9.jpg';
-exports.AD_2 = 'http://pic.yupoo.com/yige2002/DJo6EIds/PDLNL.jpg';
+exports.AD_1 = 'http://pic.yupoo.com/yige2002/DQemITQP/S0Vwt.png';
+exports.AD_2 = 'http://pic.yupoo.com/yige2002/DQemGEbS/veR4b.png';
 exports.AD_3 = 'http://pic.yupoo.com/yige2002/DP0keEDF/yC7sJ.jpg';
 exports.AD_4 = 'http://pic.yupoo.com/yige2002/DPoLyC7S/Egubu.jpg';
