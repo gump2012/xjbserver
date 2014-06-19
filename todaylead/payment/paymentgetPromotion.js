8bbb86c80f96c86e3013ff8334e9f4fbe83e9b8e
gump2012 <yige2002@tom.com>
2014-06-19 15:56:23 +0800

    修改运费逻辑

--- todaylead/payment/paymentgetPromotion.js
+++ todaylead/payment/paymentgetPromotion.js
@@ -10,27 +10,92 @@ function getPromotionDetail(response,request){
 
     var arg = url.parse(request.url).query;
     var totalprice = new Number(querystring.parse(arg).price);
-    var payment_way_id = querystring.parse(arg).payment_way_id;
+    var payment_way_id = new Number(querystring.parse(arg).payment_way_id);
+    var provice = querystring.parse(arg).province_code;
 
-    var shoping_fee = totalprice < 1000.00 ? 0.00 : 0.00;
-    var orderprice = totalprice + shoping_fee;
-    var responsevalue = {
-        info:{
-            extra:'',
-            data:{
-                product_total_price:totalprice,
-                discount_amount: 0.00,
-                price: orderprice,
-                transport_price: shoping_fee,
-                transport_price_reduce: 0.00,
-                product_price_reduce: 0.00
+    var shoping_fee = 16.0;
+
+    if(provice&&payment_way_id&&totalprice){
+        if(payment_way_id == 1 && totalprice - 20.0 < 0.01){
+            publictool.returnErr(response,'低于20元不可以货到付款哦');
+        }
+        else{
+            if(payment_way_id == 1){
+                if(totalprice - 167.99 < 0.01){
+                    switch(provice){
+                        case '2182'://山东
+                        case '1643'://江苏
+                        case '3133'://浙江
+                        case '21'://上海
+                        case '104'://安徽
+                        case '3449'://北京
+                        case '42'://天津
+                        case '814'://河北
+                        case '998'://河南
+                        case '1320'://湖北
+                        {
+                            shoping_fee = 13.0;
+                        }
+                            break;
+                        default :
+                        {
+                            shoping_fee = 16.0;
+                        }
+                            break;
+                    }
+                }else{
+                    shoping_fee = 0.0;
+                }
+            }else if(payment_way_id == 2){
+                if(totalprice - 99.99 < 0.01){
+                    switch(provice){
+                        case '2182'://山东
+                        case '1643'://江苏
+                        case '3133'://浙江
+                        case '21'://上海
+                        case '104'://安徽
+                        case '3449'://北京
+                        case '42'://天津
+                        case '814'://河北
+                        case '998'://河南
+                        case '1320'://湖北
+                        {
+                            shoping_fee = 10.0;
+                        }
+                            break;
+                        default :
+                        {
+                            shoping_fee = 13.0;
+                        }
+                            break;
+                    }
+                }else{
+                    shoping_fee = 0.0;
+                }
             }
-        },
-        response_status:'true',
-        msg:''
-    }
 
-    publictool.returnValue(response,responsevalue);
+            var orderprice = totalprice + shoping_fee;
+            var responsevalue = {
+                info:{
+                    extra:'',
+                    data:{
+                        product_total_price:totalprice,
+                        discount_amount: 0.00,
+                        price: orderprice,
+                        transport_price: shoping_fee,
+                        transport_price_reduce: 0.00,
+                        product_price_reduce: 0.00
+                    }
+                },
+                response_status:'true',
+                msg:''
+            }
+
+            publictool.returnValue(response,responsevalue);
+        }
+    }else{
+        publictool.returnErr(response,'参数不全');
+    }
 }
 
 exports.getPromotionDetail = getPromotionDetail;
