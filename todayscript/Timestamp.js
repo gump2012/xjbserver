974cbf19d27a92c52ab868e88f56e9e189c44f2e
gump2012 <yige2002@tom.com>
2014-07-03 09:25:08 +0800

    comment

--- todayscript/Timestamp.js
+++ todayscript/Timestamp.js
@@ -6,5 +6,5 @@ console.log('now:'+datanow);
 var d = new Date();
 var dd = new Date(1395557751000);
 console.log('时间戳变为时间:'+dd);
-var ddd = new Date(2014,6,2,15,46);
+var ddd = new Date(2014,5,21,13,59);
 console.log('时间变时间戳',ddd,ddd.getTime(),ddd.getMonth());
