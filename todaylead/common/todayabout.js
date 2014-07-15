c29103ecc35d430d94d556e5df409a405e68be48
gump2012 <yige2002@tom.com>
2014-07-15 09:13:37 +0800

    try again

--- todaylead/common/todayabout.js
+++ todaylead/common/todayabout.js
@@ -18,7 +18,7 @@ exports.todayabout = function(response,request){
 
     var strversion = publictool.getVersion(request);
 
-    responsevalue.info.data.content = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=gb2312" />' +
+    responsevalue.info.data.content = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' +
         '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" />' +
         '<title>关于我们</title><style>' +
         '*{margin:0;padding:0;-webkit-tap-highlight-color:rgba(0, 0, 0, 0);}body{font-size:12px;background:#F0F0F0;}.' +
@@ -31,7 +31,7 @@ exports.todayabout = function(response,request){
         'line-height:1.5em;font-size:1.25em;color:#747474;text-align:center;width:100%;}' +
         '</style></head><body><div class="logo"><div class="ver">'+strversion+'</div>' +
         '<img src="http://pic.yupoo.com/yige2002/DU9WGCQo/dvDui.png" width="64%"/></div>' +
-        '<div class="content"><div class="title"><span>商务合作方式</span></div>' +
+        '<div class="content"><div class="title"><span>'+'商务合作方式'+'</span></div>' +
         '<ul><li><span>QQ:</span><span>304261359</span></li><li><span>Email:</span><span>toupai@3pshow.com</span></li></ul></div>' +
         '<div class="footer"><div>Copyright&copy;2014 今日头牌 版权所有</div><div>三人秀(北京)科技有限公司</div></div></body></html>';
 
