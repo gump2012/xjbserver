f4e8a913839fad834ce78a39ac789825fa52ab1c
winsen <winsen@winsendeMacBook-Pro-2.local>
2014-12-05 21:43:23 -0800

    new category

--- todaylead/category/categoryGetCList.js
+++ todaylead/category/categoryGetCList.js
@@ -60,7 +60,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 1001,
 				name: "惹火睡裙",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VLnJX/dCwcP.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNFMJL1/edQOx.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -68,7 +68,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 1002,
 				name: "角色扮演",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VLdSU/UXr2V.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNFM1sv/Xmgat.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -76,7 +76,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 1003,
 				name: "情趣三点",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VLeqv/IpRut.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNFNnLv/ptblU.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -84,7 +84,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 1004,
 				name: "连体网衣",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VLyga/tiDyo.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNFMnHH/12A3qz.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -92,7 +92,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 1005,
 				name: "美腿丝袜",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VLjgE/JyDVO.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNFy4jr/pWYLR.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -100,7 +100,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 1006,
 				name: "丁字裤",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VKHKz/S82sb.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNFxNOp/bWCgg.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -108,7 +108,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 1007,
 				name: "性感肚兜",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VzQuh/aToGE.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNFNEcV/TaMdC.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -116,7 +116,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 1008,
 				name: "优雅旗袍",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VzuFc/hB71F.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNFNG0E/PatF2.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -139,7 +139,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2001,
 				name: "杜蕾斯",
-				picture: "http://pic.yupoo.com/yige2002/Ef5WxP4a/4u7q0.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGTPfn/lcr7Z.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -147,7 +147,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2002,
 				name: "冈本",
-				picture: "http://pic.yupoo.com/yige2002/Ef5WycFF/L6xTO.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGTFOF/L9q99.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -155,7 +155,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2003,
 				name: "杰士邦",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Wyae4/4vEkE.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGUax6/14XeMw.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -163,7 +163,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2004,
 				name: "名流",
-				picture: "http://pic.yupoo.com/yige2002/Ef5WyarA/SNgyZ.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGU5qz/143wwt.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -171,7 +171,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2005,
 				name: "第六感",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Wxz5A/SPdww.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGUiCr/wNUY9.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -179,7 +179,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2006,
 				name: "倍力乐",
-				picture: "http://pic.yupoo.com/yige2002/Ef5WlKRj/JNJEI.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGSXMJ/BwAk6.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -187,7 +187,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2007,
 				name: "狼牙颗粒",
-				picture: "http://pic.yupoo.com/yige2002/Ef5WyctV/oXBnk.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGVaUi/WXLU4.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -195,7 +195,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2008,
 				name: "超值促销",
-				picture: "http://pic.yupoo.com/yige2002/Ef5VzuFc/hB71F.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGTb5t/DFLjC.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -203,7 +203,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2009,
 				name: "超薄系列",
-				picture: "http://pic.yupoo.com/yige2002/Ef5WxtGX/222WJ.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGF10K/lmYRl.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -211,7 +211,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 2010,
 				name: "紧绷持久",
-				picture: "http://pic.yupoo.com/yige2002/Ef5WxZMx/O21I3.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNGV59B/EURlw.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -232,7 +232,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 3001,
 				name: "飞机杯",
-				picture: "http://pic.yupoo.com/yige2002/Ef5XCOzh/bJ0xt.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNHEOZX/N5EGh.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -240,7 +240,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 3002,
 				name: "阴臀倒模",
-				picture: "http://pic.yupoo.com/yige2002/Ef5XrsOJ/7QNHT.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNHFUHJ/kR7Ys.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -248,7 +248,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 3003,
 				name: "前列腺",
-				picture: "http://pic.yupoo.com/yige2002/Ef5XCZLG/qJLub.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNHETYc/FsCbf.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -256,7 +256,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 3004,
 				name: "充气娃娃",
-				picture: "http://pic.yupoo.com/yige2002/Ef5XCGU3/iK7z4.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNHErLU/Z7F3T.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -264,7 +264,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 3005,
 				name: "延时训练",
-				picture: "http://pic.yupoo.com/yige2002/Ef5XD9nE/9nI5m.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNHEDT5/11Xgmb.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -272,7 +272,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 3006,
 				name: "包茎矫正",
-				picture: "http://pic.yupoo.com/yige2002/Ef5XCtDJ/12E96G.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNHEmmw/13pJFj.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -294,7 +294,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4001,
 				name: "AV棒",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Y1cSL/943oV.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNHMAxU/i52RP.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -302,7 +302,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4002,
 				name: "G点刺激",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Y1nk2/V75ll.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNHMGFK/2hx2v.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -310,7 +310,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4003,
 				name: "转珠棒",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Y214p/aVnbP.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNI1E2K/ijYXD.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -318,7 +318,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4004,
 				name: "情趣跳蛋",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Y1EWZ/9HbDG.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNI1m1M/jIh16.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -326,7 +326,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4005,
 				name: "缩阴锻炼",
-				picture: "http://pic.yupoo.com/yige2002/Ef5XQd1V/Y5Met.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNI1Pje/pNTop.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -334,7 +334,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4006,
 				name: "后庭",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Y1FBn/UT6S5.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNI2eJ4/GffmP.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -342,7 +342,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4007,
 				name: "仿真阳具",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Y1F4a/8j6OP.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNI0YLp/5oQVB.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -350,7 +350,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4008,
 				name: "丰乳",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Y1Swg/F1oqQ.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNI25pw/14nWNU.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -358,7 +358,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 4009,
 				name: "穿戴",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Y1Akj/KMRGp.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNI0XNb/I0DDK.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -379,7 +379,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 5001,
 				name: "延时喷剂",
-				picture: "http://pic.yupoo.com/yige2002/Ef5YnPib/MtKkP.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIdpDr/Jyd1E.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -387,7 +387,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 5002,
 				name: "锁精套环",
-				picture: "http://pic.yupoo.com/yige2002/Ef5YnyFH/ymKE1.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIrft8/o6BYJ.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -395,7 +395,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 5003,
 				name: "延时湿巾",
-				picture: "http://pic.yupoo.com/yige2002/Ef5YnGbw/7NEEN.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIryyY/3rgmL.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -403,7 +403,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 5004,
 				name: "增大助勃",
-				picture: "http://pic.yupoo.com/yige2002/Ef5Yo9Qf/HSXGg.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIrHmq/rVxoF.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -430,7 +430,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 6001,
 				name: "私处养护",
-				picture: "http://pic.yupoo.com/yige2002/Ef5YJxfe/guqoC.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIV1I2/FGlO0.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -438,7 +438,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 6002,
 				name: "消毒清洗",
-				picture: "http://pic.yupoo.com/yige2002/Ef5YJoBT/wsjAR.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIUQOV/yQyXs.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -446,7 +446,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 6003,
 				name: "情趣润滑",
-				picture: "http://pic.yupoo.com/yige2002/Ef5YJf5X/mRA9o.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIULd8/BEZmV.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -454,7 +454,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 6004,
 				name: "可食用润滑",
-				picture: "http://pic.yupoo.com/yige2002/Ef5YJ4Br/XoGdY.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIVFg9/12Y7DL.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -462,7 +462,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 6005,
 				name: "仿真润滑液",
-				picture: "http://pic.yupoo.com/yige2002/Ef5YISHw/13KCSc.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNIUDdO/FtdcZ.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -484,7 +484,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 7001,
 				name: "调情香水",
-				picture: "http://pic.yupoo.com/yige2002/Ef60nsuh/159rdE.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJmkzs/GZjkt.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -492,7 +492,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 7002,
 				name: "女用催情",
-				picture: "http://pic.yupoo.com/yige2002/Ef60ze1o/ZZEiM.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJmB3K/4Pcd3.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -500,7 +500,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 7003,
 				name: "双人助情",
-				picture: "http://pic.yupoo.com/yige2002/Ef60zdyb/YR83l.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJmHE9/GeStG.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -520,7 +520,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 8001,
 				name: "乳头刺激",
-				picture: "http://pic.yupoo.com/yige2002/Ef60VRJX/xxHGM.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJQ3uC/13d3wV.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -528,7 +528,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 8002,
 				name: "SM系列",
-				picture: "http://pic.yupoo.com/yige2002/Ef60VGkj/nQtZr.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJBdeI/wbwwe.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -536,7 +536,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 8003,
 				name: "捆绑绳索",
-				picture: "http://pic.yupoo.com/yige2002/Ef60VGuA/DAQR8.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJPwsf/FwwF7.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -544,7 +544,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 8004,
 				name: "另类体验",
-				picture: "http://pic.yupoo.com/yige2002/Ef60VAyY/FQWRA.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJByh6/4aVa0.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -552,7 +552,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 8005,
 				name: "双人共振",
-				picture: "http://pic.yupoo.com/yige2002/Ef60KdD7/xOqMQ.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJPRIi/ajpwG.jpg"
 			}
 			item.data_list.push(subItem);
 		}
@@ -560,7 +560,7 @@ function getCategoryList_2(response,request){
 			var subItem = {
 				category_id: 8006,
 				name: "欢爱家具",
-				picture: "http://pic.yupoo.com/yige2002/Ef60VuM0/DeZKu.jpg"
+				picture: "http://pic.yupoo.com/yige2002/EfNJPNpL/1R1An.jpg"
 			}
 			item.data_list.push(subItem);
 		}
